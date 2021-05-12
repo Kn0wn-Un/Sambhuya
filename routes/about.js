cf48fd6516eb6ebb53e6f8c25511956f66751c8f
Kn0wn-Un <naikdarsh175@gmail.com>
2021-05-12 19:58:49 +0530

    mongoose connected

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('index', { title: 'Home' });
+	res.render('index', { title: 'About Us' });
+});
+
+router.get('/contact', function (req, res, next) {
+	res.render('index', { title: 'Contact Us' });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+	res.render('index', { title: 'user-home-page' });
+});
+
+router.get('/login', function (req, res, next) {
+	res.render('index', { title: 'login' });
+});
+
+router.get('/sign-up', function (req, res, next) {
+	res.render('index', { title: 'sign-up' });
 });
 
 module.exports = router;
