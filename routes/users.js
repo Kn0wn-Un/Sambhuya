f16200b4f8952418205436fd26051cece422550b
Kn0wn-Un <naikdarsh175@gmail.com>
2021-05-15 12:36:30 +0530

    user home page formatted

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,23 @@
 var express = require('express');
 var router = express.Router();
 const Post = require('../models/post');
+const user = require('../models/user');
 /* GET users listing. */
 router.get('/', function (req, res, next) {
 	if (req.user) res.redirect('/user/' + req.user._id);
 	else res.redirect('/');
 });
 router.get('/:userId', function (req, res, next) {
-	if (!req.user) res.redirect('/login');
-	Post.find({ user: req.user._id })
-		.select('description')
+	Post.find({ user: req.params.userId })
+		.populate('user')
 		.sort({ date: 1 })
 		.exec((err, posts) => {
 			if (err) return next(err);
+			var user = posts[0].user;
 			res.render('user_home_page', {
-				title: 'Home:' + req.user.name,
+				title: 'User Page:' + user.name,
 				posts: posts,
+				user: user,
 			});
 		});
 });
