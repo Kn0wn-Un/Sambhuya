949412ad11ea295c0795ff56fd72559ae7e3a6d6
Kn0wn-Un <naikdarsh175@gmail.com>
2021-05-22 01:44:34 +0530

    about us page added

--- routes/about.js
+++ routes/about.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('index', { title: 'About Us' });
-});
-
-router.get('/contact', function (req, res, next) {
-	res.render('index', { title: 'Contact Us' });
+	res.render('aboutus', { title: 'About Us' });
 });
 
 module.exports = router;
